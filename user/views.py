5652c9be706919e5d1f422284abff9a8716d4d6a
JoonPyoJang <jjpyo1009@gmail.com>
2022-09-28 20:09:47 +0900

    change views.py to model

--- user/views.py
+++ user/views.py
@@ -1,33 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import UserModel
+from django.contrib.auth import get_user_model #사용자가 있는지 검사하는 함수
 
 
 # Create your views here.
 def sign_up_view(request):
     if request.method == 'GET':
         return render(request, 'user/signup.html')
-
     elif request.method == 'POST':
         username = request.POST.get('username',None)
         password = request.POST.get('password',None)
         password2 = request.POST.get('password2',None)
         bio = request.POST.get('bio',None)
 
-
-
         if password != password2:
-            return redirect('/sign-up')
+            return render(request, 'user/singup.html')
         else:
-            exist_user = UserModel.objects.filter(username=username)
+            exist_user = get_user_model().objects.filter(username=username)
             if exist_user:
                 return HttpResponse("아이디가 존재합니다!!")
             else:
-                new_user = UserModel()
-                new_user.username = username
-                new_user.password = password
-                new_user.bio = bio
-                new_user.save()
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
                 return redirect('/sign-in')
 
 def sign_in_view(request):
